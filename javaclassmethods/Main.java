ba2061846416997f9371241dbee6e7973338a7b5
mohamed-yussuf <125584764+mohamed-yussuf@users.noreply.github.com>
2023-02-20 19:18:30 +0300

    Add files via upload
    my first commit

---
+++ javaclassmethods/Main.java
@@ -0,0 +1,19 @@
+package javaclassmethods;
+
+public class Main {
+
+    public void fullThrottle(){
+        System.out.println("Zooooooom");
+    }
+
+    public  void speed(int speed){
+        System.out.println("full speed "+speed+ "Kmh");
+
+
+
+
+        }
+
+
+    }
+
---
+++ javaclassmethods/Second.java
@@ -0,0 +1,13 @@
+package javaclassmethods;
+
+
+
+public class Second {
+    public static void main(String[] args) {
+
+        Main main= new Main();
+        main.fullThrottle();
+        main.speed(400);
+        //System.out.print(" Kmh");
+    }
+}
